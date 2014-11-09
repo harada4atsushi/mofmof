75efb87c2ece1e200ab575fa59299c89c091cb47
a.harada <redhornet96@gmail.com>
2014-11-09 14:46:46 +0900

    modify

--- lib/mofmof.rb
+++ lib/mofmof.rb
@@ -3,7 +3,7 @@ require "mofmof/version"
 module Mofmof
   class Railtie < ::Rails::Railtie
 	rake_tasks do
-	  load "tasks/stdout.rake"
+	  load "tasks/mofmof.rake"
 	end
   end
 end
--- lib/mofmof/railtie.rb
+++
@@ -1,7 +0,0 @@
-module Mofmof
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      load "tasks/stdout.rake"
-    end
-  end
-end
